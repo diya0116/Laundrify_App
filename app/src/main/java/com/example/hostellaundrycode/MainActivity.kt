0ba3fc2cd44395901564d89ccc4f159560f9e250
manviii345 <manvigajrani22@gmail.com>
2025-06-27 18:37:20 +0530

    navigation added

--- app/src/main/java/com/example/hostellaundrycode/MainActivity.kt
+++ app/src/main/java/com/example/hostellaundrycode/MainActivity.kt
@@ -1,25 +1,61 @@
-package com.example.hostellaundrycode
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hostellaundrycode.ui.theme.HostelLaundryCodeTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.hostellaundrycode.AdminLoginScreen
+import com.example.hostellaundrycode.LoginSelectorScreen
+import com.example.hostellaundrycode.StudentLoginScreen
+import com.example.hostellaundrycode.StudentSignupScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            HostelLaundryCodeTheme {
 
+        setContent {
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "selector") {
+
+                composable("selector") {
+                    LoginSelectorScreen(
+                        onStudentClick = {
+                            navController.navigate("studentLogin")
+                        },
+                        onAdminClick = {
+                            navController.navigate("adminLogin")
+                        }
+                    )
+                }
+
+                composable("studentLogin") {
+                    StudentLoginScreen(
+                        onLoginClick = { },
+                        onSignUpClick = {
+                            navController.navigate("studentSignup")
+                        }
+                    )
+                }
+
+                composable("adminLogin") {
+                    AdminLoginScreen(
+                        onLoginClick = {},
+                        onRequestAccessClick = {}
+                    )
+                }
+
+                composable("studentSignup") {
+                    StudentSignupScreen(
+                        onSignupClick = {},
+                        onLoginClick = {}
+                    )
+                }
             }
         }
     }
